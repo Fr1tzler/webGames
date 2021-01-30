93b0964afce9ad998d53cf687ebf4f861588a77e
fr1tzler <fritzlerv@yandex.ru>
2021-01-30 23:10:07 +0500

    hexatori v0.01 - added base document structure

---
+++ hexatori/script.js
@@ -0,0 +1,19 @@
+function main() {
+    resizeField();
+}
+
+function resizeField() {
+    let field = document.getElementsByClassName("field")[0];
+    let windowWidth = window.innerWidth;
+    let windowHeight = window.innerHeight;
+    if (windowHeight < windowWidth) {
+        field.style.top = windowHeight * 0.05 + "px";
+        field.style.height = windowHeight * 0.9 + "px";
+        field.style.width = windowHeight * 0.9 * 2 / Math.sqrt(3) + "px";
+    } else {
+        alert("mobile is not ready now!")
+    }
+}
+
+document.addEventListener("DOMContentLoaded", main);
+window.addEventListener("resize", resizeField);
