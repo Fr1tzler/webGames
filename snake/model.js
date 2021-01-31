6a537b822fe485622f7ab9eb505ff7a7735eaac7
fr1tzler <fritzlerv@yandex.ru>
2021-01-31 20:23:34 +0500

    snake v0.02 - model semi-final

--- snake/model.js
+++ snake/model.js
@@ -1,8 +1,8 @@
 const fruitTile = "f";
 const snakeTile = "s";
 const emptyTile = "e";
-let mapWidth;
-let mapHeight;
+let mapWidth = 12;
+let mapHeight = 12;
 let snake = [[0, 0], [0, 1], [0, 2]];
 let direction = "right";
 let map;
@@ -17,33 +17,62 @@ function generateEmptyMap() {
             map[y].push(emptyTile);
         }
     }
-    return map;    
+    return map;
 }
 
 function nextState() {
-    let snakeHead = snake.slice(snake.length - 1, snake.length);
+    let snakeHead = snake.slice(snakeLength - 1, snakeLength);
     switch (direction) {
         case "right":
+            snakeHead[0] += 1;
             break;
         case "left":
+            snakeHead[0] -= 1;
             break;
         case "up":
+            snakeHead[1] -= 1;
             break;
         case "down":
+            snakeHead[1] += 1;
             break;
     }
+    
+    snakeHead[0] = (snakeHead[0] + mapWidth) % mapWidth;
+    snakeHead[1] = (snakeHead[1] + mapHeight) % mapHeight;
+    
     switch (map[snakeHead[0]][snakeHead[1]]) {
         case fruitTile:
             snake.push(snakeHead)
             snakeLength++;
+            map[snakeHead[0]][snakeHead[1]] = snakeTile;
+            setNextFruit();
             break;
         case snakeTile:
             gameEnd = true;
             alert("FAIL");
             break;
         case emptyTile:
+            map[snakeHead[0]][snakeHead[1]] = snakeTile;
             snake.push(snakeHead)
             snake.shift()
             break;
     }
+    if (!gameEnd) {
+    }
+}
+
+// possible infinite loop
+function setNextFruit() {
+    while (true) {
+        let fruitY = getRandomInt(mapHeight);
+        let fruitX = getRandomInt(mapWidth);
+        if (map[fruitY][fruitX] == emptyTile) {
+            map[fruitY][fruitX] = fruitTile;
+            break;
+        }
+    }
+}
+
+function getRandomInt(upperBound) {
+    return Math.floor(Math.random() * upperBound);
 }
