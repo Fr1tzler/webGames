78cd76ee0408790a5dc960b1d94a3527ebf26946
fr1tzler <fritzlerv@yandex.ru>
2021-02-01 20:26:17 +0500

    snake v0.10 - first try to detect swipes

--- snake/script.js
+++ snake/script.js
@@ -99,6 +99,8 @@ function main() {
     });
     document.getElementById("startNewGame").addEventListener("click", newGame);
 
+    foo();
+
     deltaTime = 100;
     generateTiles();
     resizeMap()
@@ -241,3 +243,63 @@ document.addEventListener("keydown", function (event) {
             break;
         }
 });
+
+
+function foo() {
+    
+
+    var detector = document.getElementById("swipeDetector")
+
+    detector.addEventListener("touchstart", startTouch, false);
+    detector.addEventListener("touchmove", moveTouch, false);
+
+    // Swipe Up / Down / Left / Right
+    var initialX = null;
+    var initialY = null;
+
+    function startTouch(e) {
+    initialX = e.touches[0].clientX;
+    initialY = e.touches[0].clientY;
+    };
+
+    function moveTouch(e) {
+        if (initialX === null) {
+            return;
+        }
+
+        if (initialY === null) {
+            return;
+        }
+
+        var currentX = e.touches[0].clientX;
+        var currentY = e.touches[0].clientY;
+
+        var diffX = initialX - currentX;
+        var diffY = initialY - currentY;
+
+        if (Math.abs(diffX) > Math.abs(diffY)) {
+            // sliding horizontally
+            if (diffX > 0) {
+                // swiped left
+                console.log("swiped left");
+            } else {
+                // swiped right
+                console.log("swiped right");
+            }  
+        } else {
+            // sliding vertically
+            if (diffY > 0) {
+                // swiped up
+                console.log("swiped up");
+            } else {
+                // swiped down
+                console.log("swiped down");
+            }  
+        }
+
+        initialX = null;
+        initialY = null;
+
+        e.preventDefault();
+    }
+}
